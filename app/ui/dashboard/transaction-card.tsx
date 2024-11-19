92dc834226cdc6e51259f17718abaddb26d87721
AlanF20 <alanif@hotmail.com>
2024-11-18 22:48:22 -0600

    fix: slice the initial array

--- app/ui/dashboard/transaction-card.tsx
+++ app/ui/dashboard/transaction-card.tsx
@@ -13,13 +13,15 @@ export default function TransactionCard({amount, avatar, recurring, category, da
 	const formatedDate = dateInstance.toLocaleDateString('en-US', dateOptions )	
 	return(
 		<article className="w-full flex justify-between">
-			<div className="w-[32px] h-[32px] rounded-full overflow-clip flex items-center justify-center">
-				<img className="w-full" src={avatar} /> 
+			<div className="w-[20%]  flex items-center justify-center">
+				<img className="w-[32px] rounded-full h-[32px]" src={avatar} /> 
 			</div>
-			<p className={`${textPreset4Bold}`}>{name}</p>
-			<div className="flex flex-col justify-center items-center">
-				{isPositiveAmount ? <p className={`${textPreset4Bold} text-[--secGreen]`}>+${amount}</p> : <p className={`${textPreset4Bold} text-[--grey900]`}>-${amount}</p>}
-				<p className={`${textPreset5} text-[--grey500]`}>{formatedDate}</p>
+			<p className={`w-[50%] ${textPreset4Bold} flex items-center`}>{name}</p>
+			<div className="w-[30%] flex flex-col justify-center items-center">
+				{isPositiveAmount ? 
+					<p className={`${textPreset4Bold} text-[--secGreen]`}>+${Math.abs(amount)}</p> :
+					<p className={`${textPreset4Bold} text-[--grey900]`}>-${Math.abs(amount)}</p>}
+				<p className={`${textPreset5} text-[--grey500]`}>{formatedDate}</p>			
 			</div>
 		</article>
 	)
--- app/ui/dashboard/transactions-list.tsx
+++ app/ui/dashboard/transactions-list.tsx
@@ -4,17 +4,21 @@ import TransactionCard from "./transaction-card";
 	export function TransactionsList(){
 		return(
 			<>
-				{transactions.map(transaction => {
+				{transactions.slice(0,5).map((transaction, index) => {
+					const isLastItem = index !== 4	
 					return(
-				<TransactionCard 
-					amount={transaction.amount}
-					avatar={transaction.avatar}
-					category={transaction.category}
-					recurring={transaction.recurring}
-					name={transaction.name} 
-					date={transaction.date}/>
-
-					)	
+						<>
+						<TransactionCard 
+						key={index}
+						amount={transaction.amount}
+						avatar={transaction.avatar}
+						category={transaction.category}
+						recurring={transaction.recurring}
+						name={transaction.name} 
+						date={transaction.date}/>	
+						{ isLastItem && <div className="w-full h-[1px] bg-[--grey100]"></div>}
+					</>
+					)
 				})
 			}
 			</>
